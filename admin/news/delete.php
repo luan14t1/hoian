7be662d66d971eb86eb8c7d20416b94748cbf493
luan14t1 <taikhoantiphu.com>
2018-05-21 01:20:55 +0700

    News

---
+++ admin/news/delete.php
@@ -0,0 +1,18 @@
+<?php require_once $_SERVER['DOCUMENT_ROOT'].'/hoian/library/ConnectDatabaseLibrary.php' ?>
+<?php
+  if(isset($_GET['idNew'])){
+  $id_news = $_GET['idNew'];
+  $sql = "SELECT * FROM new WHERE id_new = '{$id_news}'";
+    $query = $conn->query($sql);
+    $news = mysqli_fetch_assoc($query);
+    $picture = $news['picture'];
+  unlink($_SERVER['DOCUMENT_ROOT'].'/hoian/uploads/images/news/'.$picture);
+  $sql1 = "DELETE FROM new WHERE id_new = {$id_news}";
+  $query1 = $conn->query($sql1);
+  if($query1){
+  	header("Location: /hoian/admin/news?msg=delsuccess");
+     }else{
+       $tb = "Xóa thất bại";
+     }
+  }
+?>
